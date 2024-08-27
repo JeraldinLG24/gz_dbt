85bc68983dc9a8337fafdf1190700f66ab8ad9bd
Jeraldin Lázaro <lazarogarciajeraldin@gmail.com>
2024-08-27 22:31:13 +0000

    commit

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -4,7 +4,7 @@
      revenue,
      quantity,
      margin,
-     margin + shipping_fee - logCost - ship_cost as operational_margin
+     (margin + shipping_fee) - (logCost - ship_cost) as operational_margin
 from
     {{ref('int_orders_margin')}}
 left join
